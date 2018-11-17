78ff5d479c3e6b91c5c28ab3554bc291628b1f08
Akito Ito <akito0107@gmail.com>
2018-11-17 18:10:51 +0900

    first

---
+++ Makefile
@@ -0,0 +1,39 @@
+.PHONY: build
+build: tools gen
+	go build -o bin/fmgo cmd/fmgo/main.go
+
+.PHONY: gen
+gen:
+	go generate ./...
+
+clean:
+	rm bin/* e2e/unienv
+
+tools: bin/generr bin/richgo
+
+bin/generr: vendor/github.com/akito0107/generr/cmd/generr
+	go build -o bin/generr ./vendor/github.com/akito0107/generr/cmd/generr
+
+bin/richgo: vendor/github.com/kyoh86/richgo
+	go build -o bin/richgo ./vendor/github.com/kyoh86/richgo
+
+vendor/github.com/kyoh86/richgo: vendor
+vendor/github.com/akito0107/generr/cmd/generr: vendor
+
+vendor:
+	dep ensure
+
+.PHONY: test
+test: test/small test/e2e
+
+.PHONY: test/small
+test/small: tools
+	bin/richgo test -v -cover
+
+
+.PHONY: test/e2e
+test/e2e: e2e/unienv tools
+	cd e2e; ../bin/richgo test -v .
+
+e2e/unienv:
+	go build -o e2e/unienv cmd/unienv/main.go
---
+++ parser.go
@@ -0,0 +1,53 @@
+package fmgo
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io"
+	"io/ioutil"
+
+	"github.com/pkg/errors"
+)
+
+//go:generate ./bin/generr -t typeNotFoundWithGivenName -i
+type typeNotFoundWithGivenName interface {
+	TypeNotFoundWithGivenName(typename string)
+}
+
+func Parse(r io.Reader, typename string) (string, *ast.TypeSpec, error) {
+	src, err := ioutil.ReadAll(r)
+	if err != nil {
+		return "", nil, errors.Wrap(err, "parse file readAll failed")
+	}
+	f, err := parser.ParseFile(token.NewFileSet(), "", string(src), parser.ParseComments)
+	if err != nil {
+		return "", nil, errors.Wrap(err, "parse file with filename is failed")
+	}
+
+	var pkgName string
+	var ts *ast.TypeSpec
+	ast.Inspect(f, func(n ast.Node) bool {
+		switch x := n.(type) {
+		case *ast.File:
+			pkgName = x.Name.Name
+		// case *ast.TypeSpec:
+		// 	if _, ok := x.Type.(*ast.InterfaceType); !ok {
+		// 		return true
+		// 	}
+		// 	if x.Name.Name == tp {
+		// 		ts = x
+		// 		return false
+		// 	}
+		default:
+			return true
+		}
+		return true
+	})
+
+	if ts == nil {
+		return "", nil, &TypeNotFoundWithGivenName{}
+	}
+
+	return pkgName, ts, nil
+}
