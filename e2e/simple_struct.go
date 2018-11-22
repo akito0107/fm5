e264f063511f6180e81996ca83e4261301eda97f
Akito Ito <akito_ito@r.recruit.co.jp>
2018-11-22 12:00:22 +0900

    feat: update readme

--- e2e/simple_struct.go
+++ e2e/simple_struct.go
@@ -2,6 +2,7 @@ package e2e
 
 import "log"
 
+//go:generate fm5 -t SimpleStruct -fo
 type SimpleStruct struct {
 	id   string
 	name string
@@ -13,4 +14,10 @@ func (*SimpleStruct) Run() {
 
 type Interface interface {
 	Run()
+}
+
+//go:generate fm5 -t Structure -fo -r Interface
+type Structure struct {
+	id   int
+	name string
 }
